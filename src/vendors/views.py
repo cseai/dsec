29b8cd86649e5df7fe8dd23c399ee37626672161
belal-bh <bh.pro.pust@gmail.com>
2020-10-18 02:01:57 +0600

    template and views updated

--- src/vendors/views.py
+++ src/vendors/views.py
@@ -4,13 +4,55 @@ from django.shortcuts import render
 
 from .forms import RegisterStoreForm
 
+from vendors.models import Store
+from addresses.models import Address
+
 @login_required
 def register_vendor_store_view(request, *args, **kwargs):
     form = RegisterStoreForm(request.POST or None, request.FILES or None)
     if request.method =='POST':
         if form.is_valid():
             # process the data in form.cleaned_data as required
-            print(form.cleaned_data)
+            title = form.cleaned_data.get('title')
+            category = form.cleaned_data.get('category')
+            opening_time = form.cleaned_data.get('opening_time')
+            closing_time = form.cleaned_data.get('closing_time')
+            
+            # address info
+            address_line_1 = form.cleaned_data.get('address_line_1')
+            address_line_2 = form.cleaned_data.get('address_line_2')
+            city = form.cleaned_data.get('city')
+            state = form.cleaned_data.get('state')
+            postal_code = form.cleaned_data.get('postal_code')
+            country = form.cleaned_data.get('country')
+
+            # create address instance
+            try:
+                address = Address.objects.create(
+                    address_type='parmanent',
+                    line_1=address_line_1,
+                    line_2=address_line_2,
+                    city=city,
+                    state=state,
+                    postal_code=postal_code,
+                    country=country
+                )
+            except:
+                raise forms.ValidationError("Invalid Address Information!")
+            
+            # create store instance
+            try:
+                store = Store.objects.create(
+                    title=title,
+                    user=request.user,
+                    category=category,
+                    opening_time=opening_time,
+                    closing_time=closing_time,
+                    address=address
+                )
+            except:
+                raise forms.ValidationError("Invalid Store Information!")
+            
             return HttpResponseRedirect('/')
     context = {
         'form': form,
