e643ee9089b24f092aa46c8685136f5c767bfd49
belal-bh <bh.pro.pust@gmail.com>
2020-10-19 03:16:32 +0600

    vendor form and views updated

--- src/vendors/forms.py
+++ src/vendors/forms.py
@@ -2,32 +2,14 @@ from django import forms
 
 from .models import Store
 
-class RegisterStoreForm(forms.Form):
-    STORE_CATEGORY_CHOICES  = Store.STORE_CATEGORY_CHOICES
-    title           = forms.CharField(label='', max_length=150)
-    category        = forms.ChoiceField(label='', widget=forms.Select(attrs={
-                    'required': True,
-                    'class': 'form-control mb-30 custom-select',
-                    'style': 'height:50px ;border-radius:0px',
-                    }),choices=STORE_CATEGORY_CHOICES)
-    opening_time    = forms.TimeField(label='', required=False)
-    closing_time    = forms.TimeField(label='', required=False)
+class RegisterStoreForm(forms.ModelForm):
     address_line_1  = forms.CharField(label='', max_length=120)
     address_line_2  = forms.CharField(label='', max_length=120, required=False)
     city            = forms.CharField(label='', max_length=120)
     state           = forms.CharField(label='', max_length=120)
     postal_code     = forms.CharField(label='', max_length=40)
     country         = forms.CharField(label='', max_length=50)
-
-    title.widget.attrs.update({
-        'placeholder': "Title", 'title': 'Store name',
-    })
-    opening_time.widget.attrs.update({
-        'placeholder': "Opening time", 'title': 'When store opens...',
-    })
-    closing_time.widget.attrs.update({
-        'placeholder': "Closing time", 'title': 'When store closes...',
-    })
+    
     address_line_1.widget.attrs.update({
         'placeholder': "Address line 1", 'title': 'Address line 1',
     })
@@ -46,21 +28,59 @@ class RegisterStoreForm(forms.Form):
     country.widget.attrs.update({
         'placeholder': "Country", 'title': 'Country',
     })
-    # class Meta:
-    #     model=Store
-    #     fields=('title','category','opening_time', 'closing_time,''address_line_1','address_line_2','city','state','postal_code','country')
-    #     labels = {
-    #         'category':''
-    #     }
-    #     widgets={
-    #         'category': forms.Select(attrs={
-    #                 'required': True,
-    #                 'class': 'form-control mb-30 custom-select',
-    #                 'style': 'height:50px ;border-radius:0px',
-    #             })
-    #     }
-    #     def __init__(self, *args, **kwargs):
-    #         super(RegisterStoreForm, self).__init__(*args, **kwargs)
-    #         self.fields['gender'].empty_label = "Select a Verb"
-    #         # following line needed to refresh widget copy of choice list
-    #         self.fields['gender'].widget.choices = self.fields['gender'].choices
+
+    class Meta:
+        model= Store
+        fields=('title', 'tagline', 'username', 'category', 'description', 'opening_time', 'closing_time', 'address_line_1', 'address_line_2', 'city', 'state', 'postal_code', 'country')
+
+        # customize form attrs
+        labels = {
+            'title': '',
+            'tagline': '',
+            'username': '',
+            'category': '',
+            'description': '',
+            'opening_time': '',
+            'closing_time': '',
+        }
+
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'placeholder': "Title",
+                'required': True,
+                'title': 'Store name',
+            }),
+            'tagline': forms.TextInput(attrs={
+                'placeholder': "Tagline",
+                'title': 'Store Tagline',
+            }),
+            'username': forms.TextInput(attrs={
+                'placeholder': "username",
+                'title': 'Unique Store username',
+            }),
+            'category': forms.Select(attrs={
+                'class': 'form-control mb-30 custom-select',
+                'style': 'height:50px ;border-radius:0px',
+                'title': 'Store Category',
+            }),
+            'description': forms.Textarea(attrs={
+                'placeholder': "Store Description",
+                'title': 'Store description',
+            }),
+            'opening_time': forms.TimeInput(attrs={
+                'placeholder': "Opening time. e.g. 8:00",
+                'title': 'Store openning time',
+            }),
+            'closing_time': forms.TimeInput(attrs={
+                'placeholder': "Closing time. e.g. 22:00",
+                'title': 'Store closing time',
+            }),
+        }
+    
+    def save(self, commit=True):
+        # Save the provided information
+        store = super(RegisterStoreForm, self).save(commit=False)
+        if commit:
+            store.save()
+        return store
+    
--- src/vendors/views.py
+++ src/vendors/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
+from django import forms
 from .forms import RegisterStoreForm
 
 from vendors.models import Store
@@ -13,10 +13,10 @@ def register_vendor_store_view(request, *args, **kwargs):
     if request.method =='POST':
         if form.is_valid():
             # process the data in form.cleaned_data as required
-            title = form.cleaned_data.get('title')
-            category = form.cleaned_data.get('category')
-            opening_time = form.cleaned_data.get('opening_time')
-            closing_time = form.cleaned_data.get('closing_time')
+            # title = form.cleaned_data.get('title')
+            # category = form.cleaned_data.get('category')
+            # opening_time = form.cleaned_data.get('opening_time')
+            # closing_time = form.cleaned_data.get('closing_time')
             
             # address info
             address_line_1 = form.cleaned_data.get('address_line_1')
@@ -29,7 +29,7 @@ def register_vendor_store_view(request, *args, **kwargs):
             # create address instance
             try:
                 address = Address.objects.create(
-                    address_type='parmanent',
+                    address_type=Address.ADDRESS_TYPE_PARMANENT,
                     line_1=address_line_1,
                     line_2=address_line_2,
                     city=city,
@@ -38,20 +38,15 @@ def register_vendor_store_view(request, *args, **kwargs):
                     country=country
                 )
             except:
-                raise forms.ValidationError("Invalid Address Information!")
+                # raise forms.ValidationError("Invalid Address Information!")
+                address = None
             
             # create store instance
-            try:
-                store = Store.objects.create(
-                    title=title,
-                    user=request.user,
-                    category=category,
-                    opening_time=opening_time,
-                    closing_time=closing_time,
-                    address=address
-                )
-            except:
-                raise forms.ValidationError("Invalid Store Information!")
+            store = form.save(commit=False)
+            if address:
+                store.address = address
+            store.user = request.user
+            store.save()
             
             return HttpResponseRedirect('/')
     context = {
