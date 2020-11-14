a40d4f1396ec3c95ceb24ab91f5606fef4f95013
belal-bh <bh.pro.pust@gmail.com>
2020-11-15 04:07:51 +0600

    express app updated

--- src/payment_accounts/models.py
+++ src/payment_accounts/models.py
@@ -6,6 +6,12 @@ from .model_utils import PaymentAccountCommonInfo
 
 User = get_user_model()
 
+################### NOTE FOR ACCOUNT ########################
+# All ACCOUNT (ONLY those are related to ACCOUNT) 
+# must end with 'Account' for the custom queryset of GFK relationship
+# i.e: 'BankAccount', 'BkashAccount' are VALID for ACCOUNT
+# and 'AccountBkash' , 'Bkash', 'Bank' are INVALID for ACCOUNT
+############################################################# 
 
 class BankAccount(PaymentAccountCommonInfo):
     user                = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
@@ -16,12 +22,18 @@ class BankAccount(PaymentAccountCommonInfo):
     account_number      = models.CharField(max_length=50)
     routing_number      = models.CharField(max_length=50)
 
+    class Meta:
+        verbose_name = "Bank Account"
+
 
 class BkashAccount(PaymentAccountCommonInfo):
     user                = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     account_type        = models.CharField(max_length=50)
     account_holder_name = models.CharField(max_length=50)
     account_number      = PhoneNumberField()
+
+    class Meta:
+        verbose_name = "bKash Account"
 
     def __str__(self):
         return f"{self.account_number.as_e164}, {self.account_type}"
