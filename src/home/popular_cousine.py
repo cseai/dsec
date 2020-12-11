34c3152d5652d57dbdb0c5d4fbf8fc667c2dff3a
Md. Shariar Kabir <kabircp08@gmail.com>
2020-12-12 01:31:41 +0600

    update mid4 in home page

---
+++ src/home/popular_cousine.py
@@ -0,0 +1,34 @@
+POPULAR_COUSINE = [
+    {
+        'name':'indian',
+        "img":'home/item.jpg'
+    },
+    {
+        'name':'fast food',
+        "img":'home/item1.jpg'
+    },
+    {
+        'name':'indian',
+        "img":'home/item.jpg'
+    },
+    {
+        'name':'fast food',
+        "img":'home/item1.jpg'
+    },
+    {
+        'name':'indian',
+        "img":'home/item.jpg'
+    },
+    {
+        'name':'fast food',
+        "img":'home/item1.jpg'
+    },
+    {
+        'name':'indian',
+        "img":'home/item.jpg'
+    },
+    {
+        'name':'fast food',
+        "img":'home/item1.jpg'
+    }
+]
